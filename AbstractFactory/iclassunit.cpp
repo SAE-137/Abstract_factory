667c15f82963a3ff038790f2dfdd15fe4837dc38
Artem Senchenko <moknechnes@mail.ru>
2025-05-10 22:52:33 +0700

    IClassUnit has been added

--- AbstractFactory/iclassunit.cpp
+++ AbstractFactory/iclassunit.cpp
@@ -1,6 +1,4 @@
 #include "iclassunit.h"
 
-IClassUnit::IClassUnit()
-{
 
-}
+
--- AbstractFactory/iclassunit.h
+++ AbstractFactory/iclassunit.h
@@ -1,11 +1,31 @@
 #ifndef ICLASSUNIT_H
 #define ICLASSUNIT_H
 
+#include <vector>
+#include <memory>
+#include <string>
+#include "AbstractProductUnit.h"
 
-class IClassUnit
-{
+class IClassUnit : public AbstractProductUnit {
 public:
-    IClassUnit();
+    using Flags = unsigned int;
+
+    explicit IClassUnit(const std::string& name) : m_name(name) {}
+    virtual ~IClassUnit() = default;
+
+
+    virtual void add(const std::shared_ptr<AbstractProductUnit>& unit, Flags flags) = 0;
+
+
+    virtual const std::vector<std::string>& getAccessModifiers() const = 0;
+
+
+    virtual std::string compile(unsigned int level = 0) const override = 0;
+
+protected:
+    std::string m_name;
+    using Fields = std::vector<std::shared_ptr<AbstractProductUnit>>;
+    std::vector<Fields> m_fields;
 };
 
 #endif // ICLASSUNIT_H

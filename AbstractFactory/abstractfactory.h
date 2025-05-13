58ef5cd58a679180552aebcfbd4f10ae2cdace5c
Artem Senchenko <moknechnes@mail.ru>
2025-05-14 01:54:29 +0700

    now the code for the cpp class matches the example from the pdf file.

--- AbstractFactory/abstractfactory.h
+++ AbstractFactory/abstractfactory.h
@@ -8,17 +8,12 @@ class AbstractFactory
 {
 public:
     virtual ~AbstractFactory() = default;
-    virtual std::shared_ptr<AbstractProductUnit> createClass(const std::string &name,
-                                                         AbstractProductUnit::Flags flags,
-                                                         AbstractProductUnit::Flags accessModifier
-                                                         ) const = 0;
+    virtual std::shared_ptr<AbstractProductUnit> createClassUnit(const std::string& name) const = 0;
 
-    virtual std::shared_ptr<AbstractProductUnit> createMethod(const std::string &name,
-                                                          const std::string &returnType,
-                                                          AbstractProductUnit::Flags flags
-                                                          ) const = 0;
-
-    virtual std::shared_ptr<AbstractProductUnit> createPrintOperator(const std::string &text) const = 0;
+    virtual std::shared_ptr<AbstractProductUnit> createMethodUnit(const std::string& name,
+                                                                  const std::string& returnType,
+                                                                  AbstractProductUnit::Flags flags) const = 0;
+    virtual std::shared_ptr<AbstractProductUnit> createPrintOperatorUnit(const std::string& text) const = 0;
 };
 
 #endif // ABSTRACTFACTORY_H
--- AbstractFactory/cppfactory.h
+++ AbstractFactory/cppfactory.h
@@ -7,28 +7,21 @@
 #include"cppmethodunit.h"
 #include"cppprintoperatorunit.h"
 
-class CppFactory : public AbstractFactory
-{
-public:
-    std::shared_ptr<AbstractProductUnit> createClass(const std::string& name,
-                                                     AbstractProductUnit::Flags /*flags*/,
-                                                     AbstractProductUnit::Flags /*accessModifier*/) const override
-    {
-        return std::make_shared<CppClassUnit>(name);
-    }
 
-    std::shared_ptr<AbstractProductUnit> createMethod(const std::string& name,
-                                                      const std::string& returnType,
-                                                      AbstractProductUnit::Flags flags) const override
-    {
-        return std::make_shared<CppMethodUnit>(name, returnType, flags);
-    }
-
-    std::shared_ptr<AbstractProductUnit> createPrintOperator(const std::string& text) const override
-    {
-        return std::make_shared<CppPrintOperatorUnit>(text);
-    }
-};
+    class CppFactory : public AbstractFactory {
+    public:
+        std::shared_ptr<AbstractProductUnit> createClassUnit(const std::string& name) const override {
+            return std::make_shared<CppClassUnit>(name);
+        }
+        std::shared_ptr<AbstractProductUnit> createMethodUnit(const std::string& name,
+                                                              const std::string& returnType,
+                                                              AbstractProductUnit::Flags flags) const override {
+            return std::make_shared<CppMethodUnit>(name, returnType, flags);
+        }
+        std::shared_ptr<AbstractProductUnit> createPrintOperatorUnit(const std::string& text) const override {
+            return std::make_shared<CppPrintOperatorUnit>(text);
+        }
+    };
 
 
 #endif // CPPFACTORY_H
